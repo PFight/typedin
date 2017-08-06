edd0905f4654d84cdb9632dc211bfd315e48b1c7
PFight <PFight77@gmail.com>
2017-08-06 21:20:21 +0300

    Fixed custom key injection.

--- src/Inject.ts
+++ src/Inject.ts
@@ -3,13 +3,13 @@
 /** Decorator, that inject value or service from DIContainer. */
 export function inject<KeyT>(context: () => Typedin.DIContainer, key?: KeyT) {
   return (target: Object, propKey: string) => {
-    const recordKey = key || Reflect.getMetadata("design:type", target, propKey);
+    let recordKey = key !== undefined ? key
+      : Reflect.getMetadata("design:type", target, propKey);
     let diRecord = context && context() && context().getRecord(recordKey);
     const descriptor = {
       get: function () {
         if (!context || !context())
           throw new Error(`Cant inject value: 'context' is not defined in decorator of property '@{propKey}'`);
-
         if (!diRecord) {
           diRecord = context().getRecord(recordKey);
         }
