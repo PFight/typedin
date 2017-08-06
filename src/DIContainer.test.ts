0bc0b5c1d9a9588ea3e0f7058fc6bb1816a53329
PFight <PFight77@gmail.com>
2017-08-06 21:15:55 +0300

    Container timestamp support + tests.

--- src/DIContainer.test.ts
+++ src/DIContainer.test.ts
@@ -14,8 +14,9 @@ class TestService {
 describe('DIContainer', () => {
   it('should return registered value', () => {
     const context = new Typedin.DIContainer();
-    context.register("SomeKey", 42);
-    expect(context.getValue("SomeKey"))
+    const key = "SomeKey";
+    context.register(key, 42);
+    expect(context.getValue(key))
       .to.equal(42);
   });
 
@@ -25,6 +26,34 @@ describe('DIContainer', () => {
     context.register(ITestService, testService);
     expect(context.getService(ITestService))
       .to.equal(testService);
+  });
+
+  it('should return registered in parent value', () => {
+    const key = "SomeKey";
+    const parentContext = new Typedin.DIContainer();
+    parentContext.register(key, 42);
+    const childContext = new Typedin.DIContainer(parentContext);
+
+    expect(childContext.getValue(key))
+      .to.equal(42);
+  });
+  it('should update timestamp on register', () => {
+    const context = new Typedin.DIContainer();
+    const initialTimestamp = context.timestamp;
+    const key = "SomeKey";
+    context.register(key, 42);
+    expect(context.timestamp)
+      .to.not.equal(initialTimestamp);
+  });
+  it('should update timestamp on register in parent context', () => {
+    const parentContext = new Typedin.DIContainer();
+    const childContext = new Typedin.DIContainer(parentContext);
+    const initialChildTimestamp = childContext.timestamp;
+
+    parentContext.register("SomeKey", 42);
+
+    expect(childContext.timestamp)
+      .to.not.equal(initialChildTimestamp);
   });
 
 });
--- src/DIContainer.ts
+++ src/DIContainer.ts
@@ -4,17 +4,12 @@ export type TypeOf<T> = Function & { prototype: T };
 
 export class DIContainer {
   private mParent: DIContainer;
-	private mMap: Typedin.DIRecord<any, any>[] = [];
+  private mChildren: DIContainer[] = [];
+  private mMap: Typedin.DIRecord<any, any>[] = [];
+  private mTimestamp: number = 0;
 
   public constructor(parent?: DIContainer) {
-    this.mParent = parent;
-  }
-
-  public get parent(): DIContainer {
-    return this.mParent;
-  }
-  public set parent(parent: DIContainer) {
-    this.mParent = parent;
+    this.parent = parent;
   }
 
   public register<KeyT, ValueT>(key: KeyT, value: ValueT): Typedin.DIRecord<KeyT, ValueT> {
@@ -25,8 +20,23 @@ export class DIContainer {
 		} else {
 			record.value = value;
     }
+    this.updateTimestamp();
     return record;
-	}
+  }
+
+  public unregister<KeyT>(key: KeyT, checkParents: boolean = false): Typedin.DIRecord<KeyT, any> {
+    let record = this.getRecord(key, checkParents);
+    if (record) {
+      record.container.mMap.splice(record.container.mMap.indexOf(record));
+    }
+    this.updateTimestamp();
+    return record;
+  }
+
+  public unregisterAll() {
+    this.mMap = [];
+    this.updateTimestamp();
+  }
 
   public getRecord<KeyT, ValueT>(key: KeyT, allowParentLookup: boolean = true): Typedin.DIRecord<KeyT, ValueT> {
     let record = this.mMap.find(x => x.key == key);
@@ -34,8 +44,45 @@ export class DIContainer {
       record = this.mParent.getRecord<KeyT, ValueT>(key);
     }
     return record;
-	}
+  }
 
+  // =======================================
+  // Parent-child and timestamp stuff
+  // ---------------------------------------
+
+  public get parent(): DIContainer {
+    return this.mParent;
+  }
+  public set parent(parent: DIContainer) {
+    let oldParent = this.mParent;
+    this.mParent = parent;
+    if (oldParent && parent != oldParent) {
+      oldParent.mChildren.splice(oldParent.mChildren.indexOf(this));
+    }
+    if (parent) {
+      this.mParent.mChildren.push(this);
+    }
+  }
+
+  public get timestamp(): number {
+    return this.mTimestamp;
+  }
+
+  private updateTimestamp() {
+    this.mTimestamp++;
+    if (this.mChildren) {
+      this.mChildren.forEach(x => x.updateTimestamp());
+    }
+  }
+
+  public dispose() {
+    this.unregisterAll();
+    this.parent = null;
+  }
+
+  // ======================================
+  // Services and values helpers
+  // --------------------------------------
 	
   public registerService<InterfaceT, ImplT>(interfaceType: InterfaceT, instance: ImplT): Typedin.DIRecord<InterfaceT, ImplT> {
     return this.register(interfaceType, instance);
