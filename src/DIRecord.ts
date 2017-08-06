52454360ebd9887aadc1732e625ed699a0868161
PFight <PFight77@gmail.com>
2017-08-06 22:43:15 +0300

    Removed DiRecord timestamp. Begun writing documentation. Added missed csproj changes to prev commit.

--- src/DIRecord.ts
+++ src/DIRecord.ts
@@ -1,9 +1,11 @@
 ﻿import * as Typedin from "./index";
 
+/**
+ * Record in {@link DiContainer}. Stores key and value.
+ */
 export class DiRecord<KeyT, ValueT> {
   private mKey: KeyT;
   private mValue: ValueT;
-  private mTimestamp: number = 0;
   private mContainer: Typedin.DiContainer;
 
   constructor(container: Typedin.DiContainer, key: KeyT, value: ValueT) {
@@ -11,20 +13,17 @@ export class DiRecord<KeyT, ValueT> {
 		this.mValue = value;
 	}
 
+  /** Key to indetify value in {@link DiContainer} */
   get key(): KeyT {
 		return this.mKey;
 	}
-	
+
+  /** Dependency value, that should be injected. */
   get value(): ValueT {
 		return this.mValue;
 	}
   set value(val: ValueT) {
 		this.mValue = val;
-		this.mTimestamp++;
-	}
-
-	get timestamp(): number {
-		return this.mTimestamp;
 	}
 
   get container(): Typedin.DiContainer {
