2cc1babbfccb81ff2c78af5fd75054ff419fcee8
PFight <PFight77@gmail.com>
2017-08-14 01:05:12 +0300

    Some working solution with guard agains container change and accessing not allowed values.

--- src/typedin.ts
+++ src/typedin.ts
@@ -1,17 +1,5 @@
 ﻿export type Requires<T> = () => T;
 
-export function bind<RequiresSpec>(r: RequiresSpec, ...p: (() => RequiresSpec)[]): Requires<RequiresSpec> {
-  return () => {
-    let result = null;
-    for (let cur of p) {
-      result = cur();
-      if (result)
-        break;
-    }
-    return result;
-  };
-}
-
 export function to<T>(arg: T | (() => T)) {
   if (typeof (arg) == "function") {
     return arg;
@@ -22,6 +10,21 @@ export function to<T>(arg: T | (() => T)) {
 
 export function injectable<T>(): T {
   return null;
+}
+
+export function bind<RequiresSpec>(spec: RequiresSpec, container: RequiresSpec): RequiresSpec {
+  let result = {};
+  for (let prop in spec) {
+    let resultProp = {
+      get: function () {
+        return container[prop];
+      },
+      configurable: false,
+      enumerable: true
+    } as TypedPropertyDescriptor<any>;
+    Object.defineProperty(result, prop, resultProp);
+  }
+  return result as RequiresSpec;
 }
 
 export function injectables<T1>(d1: T1): T1;
@@ -45,12 +48,14 @@ export function injectables<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T
 export function injectables<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19>(d1: T1, d2: T2, d3: T3, d4: T4, d5: T5, d6: T6, d7: T7, d8: T8, d9: T9, d10: T10, d11: T11, d12: T12, d13: T13, d14: T14, d15: T15, d16: T16, d17: T17, d18: T18, d19: T19): T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10 & T11 & T12 & T13 & T14 & T15 & T16 & T17 & T18 & T19;
 export function injectables<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20>(d1: T1, d2: T2, d3: T3, d4: T4, d5: T5, d6: T6, d7: T7, d8: T8, d9: T9, d10: T10, d11: T11, d12: T12, d13: T13, d14: T14, d15: T15, d16: T16, d17: T17, d18: T18, d19: T19, d20: T20): T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10 & T11 & T12 & T13 & T14 & T15 & T16 & T17 & T18 & T19 & T20;
 export function injectables() {
-  const newObj = {};
-  [].forEach.apply(arguments, (obj) => {
+  const newObj = {} as any;
+  for (let i = 0; i < arguments.length; i++) {
+    let obj = arguments[i];
     for (const key in obj) {
       //copy all the fields
       newObj[key] = obj[key];
     }
-  });
+  };
+  (newObj as From<any>).from = function (container) { return bind(this, container) };
   return newObj as any;
 }
---
+++ src/typedin3.test.ts
@@ -0,0 +1,73 @@
+﻿import { bind, to, injectable, injectables } from "./typedin";
+import { expect } from 'chai';
+import "reflect-metadata";
+
+// Interfaces
+interface ITestService {
+  foo(): string;
+}
+var $ITestService = { testService: injectable<ITestService>() };
+var $LifeMeaning = { lifeMeaning: injectable<number>() };
+var $DevilNumber = { devilNumber: injectable<number>() };
+
+// Implementation
+class TestService implements ITestService {
+  foo() {
+    return "foo";
+  }
+}
+var Global = {
+  testService: new TestService(),
+  lifeMeaning: 42,
+  devilNumber: 666
+};
+
+// Users
+class ConstructorInjectionUser {
+  static Requires = injectables($ITestService, $LifeMeaning);
+  constructor(public external: typeof ConstructorInjectionUser.Requires) {
+  }
+}
+
+
+
+describe('typedin3', () => {
+  it('should receive service and values from container', () => {
+    var container = {
+      testService: new TestService(),
+      lifeMeaning: 42,
+      devilNumber: 666
+    };
+    var user = new ConstructorInjectionUser(
+      bind(ConstructorInjectionUser.Requires, container));
+
+    expect(user.external.testService)
+      .equal(container.testService);
+    expect(user.external.lifeMeaning)
+      .equal(container.lifeMeaning);
+  });
+
+  it('should not allow to change container', () => {
+    var container = {
+      testService: new TestService(),
+      lifeMeaning: 42,
+      devilNumber: 666
+    };
+    var user = new ConstructorInjectionUser(
+      bind(ConstructorInjectionUser.Requires, container));
+
+    expect(() => user.external.lifeMeaning = 13).to.throw();
+  });
+
+  it('should not allow to access not injected values', () => {
+    var container = {
+      testService: new TestService(),
+      lifeMeaning: 42,
+      devilNumber: 666
+    };
+    var user = new ConstructorInjectionUser(
+      bind(ConstructorInjectionUser.Requires, container));
+
+    expect(user.external["devilNumber"]).equals(undefined);
+  });
+});
